46325fc6002a93a3ec68da71d9508a83bfd5ca10
mlilu <lilumontoya.net@gmail.com>
2024-05-19 17:48:46 -0700

    updated functional todo list

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ import { TodoList } from "./TodoList";
 import "./styles.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(title) {
     setTodos((currentTodos) => {
--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export function NewTodoForm({ onSubmit }) {
-  props.onSubmit;
   const [newItem, setNewItem] = useState("");
 
   function handleSubmit(e) {
